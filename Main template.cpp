716cc3ec6c5457691b9ddb6f51d532fa4294332d
Mohammed Waleed <136664646+Mohammed-Waleed061126@users.noreply.github.com>
2024-07-19 22:09:21 +0300

    Add files via upload

---
+++ Main template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define el "\n"
+#define yes cout<<"YES"<<el;
+#define no cout<<"NO"<<el;
+#define MW_YH ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+void solution(){
+  
+}
+
+int main(){ MW_YH
+  //freopen("input.txt","r",stdin);
+  //freopen("output.txt","w",stdout);
+  int ntimes = 1;
+  cin >> ntimes;
+  while (ntimes--)
+  {
+    solution();
+  }
+}
